efeab0517f8d77dabec84b2d4063ccad73a0ef93
Phanitha Sree <andra.phanitha-sree@capgemini.com>
2024-02-06 09:52:35 +0530

    first commited files added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Nav from '../src/components/commonComponents/nav'
+import Sidebar from './components/commonComponents/sidebar';
+import Footer from './components/commonComponents/footer';
+// import Sapling from '../src/assets/nature/Sapling.png'
+import Home from './components/commonComponents/home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './components/login'
+import Bonsai from './components/bonsai';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className="grid-container">
+      <Nav />
+      <Sidebar />
+      <div className="item3" >
+        <Router>
+          <Routes>
+            <Route path="/Login" element={<Login />} />
+            <Route path="*" element={<Home />} />
+            <Route path="/Bonsai" element={<Bonsai />} />
+          </Routes>
+        </Router>
+
+      </div>
+      <Footer />
     </div>
+
+
+
   );
 }
 
